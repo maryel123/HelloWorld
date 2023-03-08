210be3ae82d211302d171ada8d30450bb9d49c74
MARIELLE <mariellezbl@gmail.com>
2023-03-08 23:25:50 +0800

    banong code

--- src/Atm.java
+++ src/Atm.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-    public class Atm {
+public class Atm {
 
     public static String accounts[][] = new String[3000][4], accountNumber = " ", accountName = " ";
-
-    public static int pinTrial = 3;
+    //VARIABLES
+    public static int pinTrial = 3, currentuser = -1;
     public static double balance = 0, withdrawn = 0;
+
 
     public static void main(String[] args) {
 
-        //VARIABLES
+
         accounts[0][0] = "0123-4567-8901";
         accounts[0][1] = "John Doe";
         accounts[0][2] = "5000.00";
@@ -24,9 +26,6 @@ import java.util.Scanner;
         accounts[2][1] = "Jessica Martinez";
         accounts[2][2] = "10000";
         accounts[2][3] = "3333";
-
-
-
 
         pinEntering();
     }
@@ -43,31 +42,60 @@ import java.util.Scanner;
         for (int i = 0; i < accounts.length; i++) {
 
             //IF STATEMENT
+            //CONTINUE
+
+
+            //NESTED IF ELSE STATEMENT
             if (pin.equals(accounts[i][3])) {
                 accountNumber = accounts[i][0];
                 accountName = accounts[i][1];
                 balance = Double.parseDouble(accounts[i][2]);
-                choosingOfTransactions();}
 
 
+                if (pin.equals("1111")) {
+                    currentuser = 0;
+                } else if (pin.equals("2222")) {
+                    currentuser = 1;
+                } else if (pin.equals("3333")) {
+                    currentuser = 2;
+                }
 
+                choosingOfTransactions();
+
+            }
+            //DO WHILE
+
+                do {
+
+                    pinTrial = pinTrial - 1;
+                    System.out.println("Invalid pin number. \nPlease try again. ");
+                    System.out.println("You have " + pinTrial + " attempts left.");
+                    System.exit(0);
+
+                    //IF STATEMENT
+                    //CONTINUE
+                    if (pin.equals("0000")) {
+                        continue;
+
+                    }
+
+                } while (pin != accounts[currentuser][3]);
 
         }
 
 
 
 
+
+
+
+
+
+
     }
 
-
-
-
-
-
-
-
     private static void choosingOfTransactions() {
-        //SWITCH
+
         char transaction;
 
         Scanner t = new Scanner(System.in);
@@ -75,13 +103,14 @@ import java.util.Scanner;
         System.out.print("Choose Transaction: \n C-check balance \n W-withdraw  \n D-deposit \n E-exit  \n  ");
         transaction = t.next().charAt(0);
 
+        //SWITCH
+        //BREAK
         switch (Character.toUpperCase(transaction))
         {
 
             case 'C':
                 checkingOfBalance();
                 break;
-
 
             case 'W':
                 withdraw();
@@ -92,7 +121,10 @@ import java.util.Scanner;
                 break;
 
             case 'E':
-                System.exit(0);
+                System.out.println("Thank you for using Marielle's Atm Machine. Please come back again.");
+
+                pinEntering();
+                currentuser = -1;
                 break;
 
         }
@@ -100,13 +132,21 @@ import java.util.Scanner;
 
 
 
-
     private static void checkingOfBalance() {
+
+        String answer;
+
+        System.out.println("You currently have " + accounts[currentuser][2] + " pesos ");
+
+
+        Scanner d = new Scanner(System.in);
+        System.out.println("\n Would like to choose another transaction?(Yes/No) ");
+        answer=d.nextLine();
+
         //TERNARY
-        String result = (balance > 0) ? "Sufficient balance" : "Inufficient balance" ;
-
-        System.out.println("You have" + result);
-
+        String result = (answer.equalsIgnoreCase("Yes")) ? "Rerun the program and enter your pin." : "Thank you for using Marielle's Atm Machine.";
+        System.out.println(result);
+        System.exit(0);
 
     }
 
@@ -114,51 +154,57 @@ import java.util.Scanner;
         int withdrawnAmount;
         Scanner b = new Scanner(System.in);
         System.out.println("Please input amount to withdraw:");
-        withdrawnAmount=b.nextInt();
+        withdrawnAmount = b.nextInt();
+
+        //IF ELSE
+            if (withdrawnAmount > balance) {
+                System.out.println("Insufficient Balance");
+                System.exit(0);
+            } else if (withdrawnAmount < 100) {
+                System.out.println("Invalid amount");
+                System.exit(0);
+            } else {
+
+                balance = balance - withdrawnAmount;
+                System.out.println("Transaction successful. Please get your cash.");
+                System.exit(0);
+            }
 
 
-        if (withdrawnAmount< balance) {
-            System.out.println("Insufficient Balance");
         }
-        else if (withdrawnAmount<100) {
-            System.out.println("Invalid amount");
-        }
-
-
-        else {
-
-            balance=balance-withdrawnAmount;
-            System.out.println("Transaction successful. Please get your cash.");
-            System.exit(0);
-        }
-
-
-
-    }
 
 
     private static void deposit() {
 
         // WHILE LOOP
-            int depositedAmount;
-            Scanner b = new Scanner(System.in);
+        float depositedAmount = 0f;
+        Scanner b = new Scanner(System.in);
+        System.out.println("Please input amount to deposit:");
+        depositedAmount=b.nextFloat();
+
+        while(depositedAmount <=0) {
+            System.out.println("Invalid Amount");
             System.out.println("Please input amount to deposit:");
-            depositedAmount=b.nextInt();
-
-            while(depositedAmount <=0) {
-                System.out.println("Invalid Amount");
-                System.out.println("Please input amount to deposit:");
-                depositedAmount=b.nextInt();
-
+            depositedAmount=b.nextFloat();
         }
-
-
-        balance= balance + depositedAmount;
-        System.out.println("Your new balance is:" + balance);
+        accounts[currentuser][2] = String.valueOf(depositedAmount + Float.parseFloat(accounts[currentuser][2]));
+        System.out.println("Your new balance is:" + accounts[currentuser][2]);
         System.out.println("Transaction Successful. Thank you." );
-
         choosingOfTransactions();
+    }
 
+    //when the users Exit
+    // Garbage Collection Applied
+    private static void exitMethod() {
+        //clear the income and outcome Arraylist
+        accounts[][].clear();
+
+        //garbage collection to free up memory
+        System.gc();
+
+        //printing
+        System.out.println("Garbage collection completed");
+        System.exit(0);
     }
 
 
